995cef74d55fb418a89932c2d7b3ac266b6bc35e
Blaise Cador <blaise.cador@gmail.com>
2017-02-23 16:31:56 +0100

    add mqtt to arduino

---
+++ mqtt_serial.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import time
+import serial
+import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as subscribe
+
+#brokerIp = "193.70.0.25"
+#brokerPort = "1334"
+
+brokerIp = "10.3.5.41"
+brokerPort = 1883
+
+color = ""
+ser = serial.Serial('/dev/ttyACM1', 9600, timeout=0,  parity=serial.PARITY_EVEN, rtscts=1)
+print("Writing on %s..." % ser.name)
+
+while(1):
+  msg = subscribe.simple("/iot", qos=0, msg_count=1, retained=False, hostname=brokerIp, port=brokerPort, client_id="", keepalive=60, will=None, auth=None, tls=None, protocol=mqtt.MQTTv311)
+
+  msgcolor = msg.payload
+  
+  if(len(msgcolor) == 1):
+    print msgcolor
+    if(msgcolor == "3"):
+      color = "r"
+    if(msgcolor == "9"):
+      color = "b"
+    if(msgcolor == "E"):
+      color = "y"
+    if(msgcolor == "D"):
+      color = "w"
+print color
+ser.write(color)
+ 
+time.sleep(1)
--- serial_mqtt.py
+++ serial_mqtt.py
@@ -3,21 +3,30 @@ import time
 import serial
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+
+#brokerIp = "193.70.0.25"
+#brokerPort = "1334"
+
+brokerIp = "10.3.5.41"
+brokerPort = 1883
+
 color = ""
 ser = serial.Serial('/dev/ttyACM1', 9600, timeout=0,  parity=serial.PARITY_EVEN, rtscts=1)
 print("Listening on %s..." % ser.name)
+
+#s = "3"
 
 while(1):
   s = ser.read(100)
   if(len(s) == 1):
     print s
     if(s == "3"):
-      color = "RED"
+      color = "r"
     if(s == "9"):
-      color = "BLUE"
+      color = "b"
     if(s == "E"):
-      color = "YELLOW"
+      color = "y"
     if(s == "D"):
-      color = "WHITE"
-    publish.single("/iot", payload=color, qos=0, retain=False, hostname="10.3.5.41", port=1883, client_id="", keepalive=60, will=None, auth=None, tls=None, protocol=mqtt.MQTTv311, transport="tcp")
+      color = "w"
+    publish.single("/iot", payload=color, qos=0, retain=False, hostname=brokerIp, port=brokerPort, client_id="", keepalive=60, will=None, auth=None, tls=None, protocol=mqtt.MQTTv311, transport="tcp")
   time.sleep(1)
